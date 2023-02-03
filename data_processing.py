4820e8be852f1023978cb6f79bff9b92ce0a0878
dong99u <86235780+dong99u@users.noreply.github.com>
2023-02-04 03:26:29 +0900

    +gitignore

--- data_processing.py
+++ data_processing.py
@@ -1,13 +1,13 @@
 import csv
 
-def save_to_csv(review_list):
+def save_to_csv(review_list, file_name):
     try:
-        with open('final_project/movie_reviews.csv', 'w', encoding='utf-8', newline='') as file:
+        with open(file_name, 'w', encoding='utf-8', newline='') as file:
             writer = csv.writer(file)
             writer.writerow(['title', 'score', 'comment', 'user_id', 'date'])
             for review in review_list:
                 for elem in review:
                     writer.writerow([elem['title'], elem['score'], elem['comment'], elem['user_id'], elem['date']])
                 
-    except:
-        print('Error: Can\'t save to csv file')
+    except Exception as e:
+        print(f'Error: Can\'t save to csv file: {e}')
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import extractor.scraping as scraping
 import data_processing
 
-if __name__ == '__main__':
-    reviews = scraping.get_movie_reviews(kind='all_time')
+def main():
+    reviews = scraping.get_movie_reviews(kind='now')
 
-    data_processing.save_to_csv(reviews)
+    data_processing.save_to_csv(reviews, 'final_project/movie_reviews_now.csv')
+
+if __name__ == '__main__':
+    main()
