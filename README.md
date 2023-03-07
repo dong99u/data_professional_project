66d97c7d1b532996ee0839762b4736ada2027af9
Park Dongkyu <qkrehdrb0813@gmail.com>
2023-03-07 21:58:44 +0900

    최종
    최종

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # final_project
- Final project in HUFS data science for naver movie
+
+Final project in HUFS data science for naver movie
+
+## Google Colab 환경에서 실행함.
+
+labeled_data_analyze.ipynb 파일은 torchtext로 다운로드 받은 긍정/부정 두 가지로 미리 라벨링된 Dataframe(train_data)을 통해 로지스틱 분류 모델에 학습시켜
+test_data와 비교해본다.
+
+naver_movie_review.ipynb 파일은 실제 영화 리뷰 텍스트와 평점을 학습하여 결과를 도출함.
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import extractor.scraping as scraping
 import data_processing
 
-def main():
+def scrape_data():
+    """ 데이터를 다운로드한다. 현재 디렉터리에 data폴더에 저장."""
     # 20230206 기준 영화 최대 10페이지까지의 리뷰를 스크래핑해서 csv 파일로 저장
     scraping.movie_reviews(kind='all_time', page=10, date='20230206')
     scraping.movie_reviews(kind='now', page=1, date='20230206')
@@ -10,6 +11,8 @@ def main():
     scraping.movie_short_comments(kind='now', page=1, date='20230206')
 
 
+def main():
+    scrape_data()
     
 
 
